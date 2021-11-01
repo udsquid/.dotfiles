f1fdf65ce00dea50d3d7fc274108670ae483f55d
lab <lab@neil-imac.local>
2021-11-01 17:16:28 +0800

    Add the reason about why we need to search all the files in 'GTD'

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -234,9 +234,9 @@
 
 (defun org-setup-agenda ()
   (require 'find-lisp)
+  ;; also want to see done tasks in 'Archive.org'
   (setq org-agenda-files
-	'("~/Dropbox/mywiki/GTD/Inbox.org"
-	  "~/Dropbox/mywiki/GTD/Next.org"))
+	(find-lisp-find-files (concat org-directory "/GTD") "\.org$"))
   (setq org-agenda-custom-commands
 	`(("g" "Agenda Overview"
 	   ((agenda ""
